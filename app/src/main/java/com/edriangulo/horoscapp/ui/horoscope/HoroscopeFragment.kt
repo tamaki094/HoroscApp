d314726fd52bae5effddb3de3f93014e61905381
tamaki094 <edri.alan.94@gmail.com>
2025-02-06 15:22:09 -0600

    Firs commit

---
+++ app/src/main/java/com/edriangulo/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,25 @@
+package com.edriangulo.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.edriangulo.horoscapp.R
+import com.edriangulo.horoscapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
